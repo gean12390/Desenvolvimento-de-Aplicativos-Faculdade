79f80bfe637d90f11b0387a0ccdf4108baf7fd7c
Giovanni Pascuotte da Silva <100391366+gean12390@users.noreply.github.com>
2025-02-07 10:51:26 -0300

    Create Main.kt

---
+++ Segundo-Codigo/Main.kt
@@ -0,0 +1,14 @@
+
+fun main(){
+    val a = 14;
+    val b = 5;
+    val c = 7;
+    val d = 2;
+    
+    val a1 = (a / b) * (c % d)
+    val a2 = (a - c) / (b * d)
+    val a3 = (b * d) - (b+ (d % c))
+
+    println((a1* a2) - a3)
+    
+}
