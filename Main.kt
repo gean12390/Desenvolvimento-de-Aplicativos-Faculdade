cccdf74833615114bbb6f0e759e7a2113c8d4332
Giovanni Pascuotte da Silva <100391366+gean12390@users.noreply.github.com>
2025-02-09 12:53:20 -0300

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,15 @@
+fun main(){
+    val a: Int = "8".toInt();
+    val b: Int = "5".toInt();
+    val c: Int = a + b;
+    val d: Long = c.toLong() + 5L
+
+    val e: Float = "1.5".toFloat();
+    val f: Double = e.toDouble() * 0.5 + d 
+    
+    val g = (f/2).toInt()
+    val r = (d/c) + (d / b) - (g % a)
+
+    println(r); 
+
+    }
