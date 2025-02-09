047f085585510f7a5b3ee94a5645c8098a61f695
Giovanni Pascuotte da Silva <100391366+gean12390@users.noreply.github.com>
2025-02-09 12:54:10 -0300

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,15 @@
+fun main(){
+    val numbers1 = arrayOf(1, 2, 3, 4, 5); 
+    val numbers2 = arrayOf(10, 20, 30, 40, 50, 60, 70);
+
+    var r = 0;
+    var i = 0;
+
+    while(i < numbers1.size && i < numbers2.size){
+        if(i % 2 == 0){
+            r += numbers1[i] + numbers2[i];
+        }
+        i++;
+    }
+    println(r);
+}
